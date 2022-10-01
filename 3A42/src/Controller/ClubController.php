14d69a65954bb74c54c2b022667fb44854a2152b
[alouaneR] <[rym.alouane@esprit.tn]>
2022-10-01 09:18:30 +0100

    AtelierTwig 27-09

---
+++ 3A42/src/Controller/ClubController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ClubController extends AbstractController
+{
+    #[Route('/club', name: 'app_club')]
+    public function index(): Response
+    {
+        return $this->render('club/index.html.twig', [
+            'controller_name' => 'TWIG',
+        ]);
+    }
+
+        #[Route('/get/{nom}', name: 'app_club1')]
+        public function getName($nom): Response
+        {
+            return $this->render('club/detail.html.twig', [
+                'n' => $nom
+            ]);
+        }
+
+  #[Route('/list', name: 'app_club2')]
+    public function list()
+    { $listTableau = array(
+        array('ref' => 'f1', 'Titre' => 'Formation Symfony 4','Description'=>'formation pratique','nb_participants'=>19) ,
+        array('ref'=>'f2','Titre'=>'Formation SOA' ,'Description'=>'formation theorique','nb_participants'=>0),
+        array('ref'=>'f3','Titre'=>'Formation Angular' ,'Description'=>'formation theorique','nb_participants'=>12)
+
+    );
+
+
+        return $this->render('club/affiche.html.twig',[
+            'listeTableau'=> $listTableau
+        ]);
+    }
+
+   #[Route('/detail/{id}', name: 'app_club3')]
+    public function detail($id)
+    {return $this->render('club/detail.html.twig', [
+            'controller_name' => 'ClubController','nom'=>$id
+        ]);
+    }
+}
